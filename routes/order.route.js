b6426535f49bbd81e83131e4b66a14bab65e4238
Syed Shayan Ali Qadri <syedshayanaliqadri@gmail.com>
2025-08-01 18:09:13 +0500

    Update order.route.js

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/jwt.js";
-import { getOrders, intent, confirm } from "../controllers/order.controller.js";
+import { createOrder, getOrders, intent, confirm } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ const router = express.Router();
 router.get("/", verifyToken, getOrders);
 router.post("/create-payment-intent/:id", verifyToken, intent);
 router.put("/", verifyToken, confirm);
+router.post("/", verifyToken, createOrder); 
+
 
 export default router;
